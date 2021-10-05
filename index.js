dd96518d7d5fee53a17345d1897697cccf19d413
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-05 14:50:48 +0200

    Javascript exercise

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const genres = ["Comedy", "Drama", "Romance", "Horror", "Sci-Fi", "Documentary"]
 
 
 //2) Create an unordered list using JavaScript and save it in a variable
-
+const unorderedList = document.createElement("ul")
 
 //3) Cycle the array and create a list-item via JavaScript for every element
 
 let displayAsAList = function(arr) {
-    let element;
-    for (i = 0; i < genres.length; i++) {
-        //displayArrayAsHtmlList(genres[i], element)
+
+    for (i = 0; i < arr.length; i++) {
+        //displayArrayAsHtmlList(arr[i], element)
         const listItems = createElement("li")
         listItems.innerHTML = arr[i]
     }
     return element
 }
-console.log(displayAsAList(genres))
-    //4) Insert the genere as text in the list-item
+
+//4) Insert the genere as text in the list-item
+displayAsAList(genres)
     //5) Append the list items in the unordered list
-    //6) Append the unordered list in the document. The list should appear in the page. */
+unorderedList.appendChild(listItems)
+    //6) Append the unordered list in the document. The list should appear in the page.
+document.appendChild(unorderedList)
